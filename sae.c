5fc6c3cfc015185202b8ca8a7270fb38194525e2
ghassan.jabbour <ghassan.jabbour@etu.uca.fr>
2024-10-16 09:10:49 +0200

    tiens

--- sae.c
+++ sae.c
@@ -1,1 +1,24 @@
 #include "sae.h"
+
+void remplirtableau(int etu[], int stage[])
+{   
+    int i;
+    FILE *flot, *stage;
+    flot=fopen("etudiants.txt", "r");
+    if (flot==NULL)
+    {
+        printf("Probleme d'ouverture de fichier");
+        return -1;
+    }
+    stage=fopen("stage.txt","w");
+    if (stage==NULL)
+    {
+        printf("Probleme d'ouverture de fichier");
+        return -1;
+    }
+    while ()
+    {
+        
+    }
+    
+}
